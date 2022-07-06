074f6270eb2aa294d57fe728ec68c547df0d55ab
Vasily Lusnikov <66748865+papanage@users.noreply.github.com>
2022-07-06 23:24:14 +0700

    #issue-3: Добавить CI/CD  (#11)
    * [TASK-34734]: Мигрирую на self-hosted раннер

---
+++ src/test/java/com/intabia/wikitabia/rest/DefaultTest.java
@@ -0,0 +1,13 @@
+package com.intabia.wikitabia.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DefaultTest {
+
+  @Test
+  void whenContextLoad__thenSuccess() {
+    System.out.println("Тестируем...");
+  }
+}
